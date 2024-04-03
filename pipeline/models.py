db1c91e730395e7b09dd968e3f2867a6d93795c5
960ptou <374780732weijie@gmail.com>
2024-04-03 18:50:03 -0400

    models file docstring

--- pipeline/models.py
+++ pipeline/models.py
@@ -1,3 +1,6 @@
+"""Models and Parameters loading
+"""
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -5,10 +8,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 
-
-
+# Adding models
 models = []
-
 models.append(LogisticRegression(solver='liblinear'))
 models.append(SVC())
 models.append(KNeighborsClassifier(n_neighbors=7))
@@ -16,15 +17,16 @@ models.append(DecisionTreeClassifier())
 models.append(RandomForestClassifier())
 models.append(GaussianNB())
 
-param_grids = [
-    {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'penalty': ['l1', 'l2']}, # Logistic Regression
-    {'C': [1, 10, 100, 1000], 'kernel': ['linear', 'rbf'], 'gamma': [0.1, 0.01, 0.001]}, # SVC
-    # {'C': [1, 10, 100, 1000], 'kernel': ['linear', 'rbf'], 'gamma': [0.1, 0.01, 0.001]}, # SVC
-    {'n_neighbors': [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23], 'metric': ['euclidean', 'manhattan', 'chebyshev', 'minkowski'], 'weights': ['uniform', 'distance']}, # KNeighborsClassifier
-    {'max_depth': [None, 5, 10, 15, 20, 25, 30], 'min_samples_split': [2, 5, 10, 15, 20], 'min_samples_leaf': [1, 2, 4, 6, 8]},# DecisionTreeClassifier
-    {'criterion': ['gini', 'entropy'], 'n_estimators': [100, 150, 200, 250, 300]},# RandomForestClassifier
-    {}  # GaussianNB doesn't have hyperparameters
-]
+# adding param grid and ensuring order match
+param_grid_dict = {
+    "LogisticRegression" : {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'penalty': ['l1', 'l2']},
+    "SVC" : {'C': [1, 10, 100, 1000], 'kernel': ['linear', 'rbf'], 'gamma': [0.1, 0.01, 0.001]},
+    "KNeighborsClassifier" : {'n_neighbors': [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23], 'metric': ['euclidean', 'manhattan', 'chebyshev', 'minkowski'], 'weights': ['uniform', 'distance']},
+    "DecisionTreeClassifier" : {'max_depth': [None, 5, 10, 15, 20, 25, 30], 'min_samples_split': [2, 5, 10, 15, 20], 'min_samples_leaf': [1, 2, 4, 6, 8]},
+    "RandomForestClassifier" : {'criterion': ['gini', 'entropy'], 'n_estimators': [100, 150, 200, 250, 300]},
+    "GaussianNB" : {} # GaussianNB doesn't have hyperparameters
+}
 
-if __name__ == "__main__":
-    test_model = SVC()
+param_grid = [
+    param_grid_dict[model.__class__.__name__] for model in models
+]
