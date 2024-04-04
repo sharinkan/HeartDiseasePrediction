8f950567db443c10cc30c47fb4d5dfeac207e1c4
960ptou <374780732weijie@gmail.com>
2024-04-03 20:05:24 -0400

    pipeline docstring

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,20 +4,34 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from .models import param_grids
+import numpy as np
 
-
+from typing import Tuple, List, Dict
 def one_dim_x_train(
-        X, 
-        y,
-        models, # SVC() kind things, 
+        X : np.ndarray, # each data is 1D 
+        y : np.ndarray,
+        models : "MLmodel",
         test_size: float, 
-        random_state = None,
-    ):
-    # test train split
+        random_state : int = None,
+    ) -> Tuple[list, list, list, List[np.ndarray, np.ndarray]]:
+    """training models with 2D X 
 
+    Args:
+        X (np.ndarray): training data
+        y (np.ndarray): training data label
+        models (MLmodel): models
+        test_size (float): test size by percentage
+        random_state (int, optional): random seed. Defaults to None.
+
+    Returns:
+        Tuple[list, list, list, List[np.ndarray, np.ndarray]]: accuarcy list, AUC list, confusion matrix list, [testing X, testing Y]
+    """
+    
+
+    # normalization
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
-
+    # test train split
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size = test_size, random_state=random_state)
     acc_list, auc_list, cm_list = [],[],[]
     
@@ -38,14 +52,27 @@ def one_dim_x_train(
     return acc_list, auc_list, cm_list, (X_test, y_test)
 
 def mixture_one_dim_x_train(
-        Xs : dict,  # feat_name : feat_ary
-        ys : dict, # feat_name : feat_ary
-        models_feat : dict, # model : feat_name
+        Xs : Dict["Feature Name", np.ndarray], 
+        ys : Dict["Feature Name", np.ndarray],
+        models_feat : Dict["MLmodel", "Feature Name"],
         test_size: float, 
         random_state = 1, # make this fixed, or it might not work too well.. as it might give some model trained data as testing data
         grid_search_enabled = False
-    ):
-    # test train split
+    )-> Tuple[list, list, list, List[np.ndarray, np.ndarray]]:
+    """training with each model with different feature sets
+
+    Args:
+        Xs (Dict["Feature Name", np.ndarray]): training Xs by feature name
+        ys (Dict["Feature Name", np.ndarray]): training Ys by feature name
+        models_feat (Dict["MLmodel", "Feature Name"]): models in dictionary by feature name
+        test_size (float): test size by percentage
+        random_state (int, optional): random seed. Defaults to 1.
+
+    Returns:
+        Tuple[list, list, list, List[np.ndarray, np.ndarray]]: accuarcy list, AUC list, confusion matrix list, [testing X, testing Y]
+    """
+    
+
 
     acc_list, auc_list, cm_list = [],[],[]
     test_Xs, test_ys = {}, {}
